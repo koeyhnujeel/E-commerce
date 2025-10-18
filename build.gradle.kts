fb5923997d2203bb6fc318a14ebe5c137932e73e
zunza <fksfks015@naver.com>
2025-10-19 01:08:09 +0900

    feat: 로깅 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("io.jsonwebtoken:jjwt:0.12.6") // JWT
+	implementation("io.github.oshai:kotlin-logging-jvm:7.0.3") // Kotlin-Logging
 
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
